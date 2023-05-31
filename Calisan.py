73c9860b02eae556ea6ff2a8ca33306cf177ae7d
mSalihAkcck <115043435+mSalihAkcck@users.noreply.github.com>
2023-06-01 02:08:52 +0300

    Calisan class'ında metotlar yazıldı

---
+++ Calisan.py
@@ -0,0 +1,45 @@
+import Insan
+
+class Calisan(Insan.Insan):
+
+    def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, tecrube, maas, sektor):
+        Insan.Insan.__init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk)
+        self.__tecrube = tecrube
+        self.__maas = maas
+        self.__sektor = sektor
+
+    def set_maas(self, maas):
+        self.__maas = maas
+    def get_maas(self):
+        return self.__maas
+    def get_tecrube(self):
+        return self.__tecrube
+    def get_sektor(self):
+        return self.__sektor
+
+    def zam_hakki(self):
+        try:
+            while self.__sektor not in ["Teknoloji", "muhasebe", "insaat", "diger"]:
+                print(f"{Insan.Insan.get_ad(self)} çalışanının sektörü hatalı girilmiştir.")
+                break
+
+            if self.get_tecrube() < 24:
+                self.set_maas(self.get_maas())
+            elif (24 <= self.get_tecrube() <= 48) and self.get_maas() < 15000:
+                zam_orani = self.get_maas() % self.get_tecrube()
+                yeni_maas = int(self.get_maas() + (self.get_maas()*zam_orani/100))
+                self.set_maas(yeni_maas)
+            elif self.get_tecrube() > 48 and self.get_maas() < 25000:
+                zam_orani = (self.get_maas() % self.get_tecrube()) / 2
+                yeni_maas = int(self.get_maas() + (self.get_maas()*zam_orani/100))
+                self.set_maas(int(yeni_maas))
+
+        except Exception:
+            print("Zam hesaplanırken hata oluştu işlem adımlarını kontrol ediniz.")
+
+    def __str__(self):
+        Calisan.zam_hakki(self)
+        return f"Ad: {Insan.Insan.get_ad(self)}\n" \
+               f"Soyad: {Insan.Insan.get_soyad(self)}\n" \
+               f"Tecrübe: {self.get_tecrube()} ay\n" \
+               f"Yeni maaş: {self.get_maas()} TL\n"
